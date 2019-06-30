8b5827b8260da7fb470406a20c595dee31135023
José Miguel Gonçalves <jose.goncalves@inov.pt>
2019-07-01 00:12:47 +0100

    Escape double brackets.

--- template/src/components/HelloWorld.vue
+++ template/src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="hello">
-		<h1>{{ msg }}</h1>
+		<h1>\{{ msg }}</h1>
 		<h3>Essential Links</h3>
 		<ul>
 			<li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
